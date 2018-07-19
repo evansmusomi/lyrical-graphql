dbd2a6d65c61479fdb029fa539b9bbaf67bfadb3
Evans Musomi <evans.musomi@gmail.com>
2018-07-19 07:35:33 +0300

    feat: adds navigation links on song list and create song components and navigates between the two when a song is created

--- client/components/SongCreate.js
+++ client/components/SongCreate.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { graphql } from "react-apollo";
 import gql from "graphql-tag";
+import { Link, hashHistory } from "react-router";
 
 class SongCreate extends Component {
   constructor(props) {
@@ -10,16 +11,19 @@ class SongCreate extends Component {
 
   onSubmit() {
     event.preventDefault();
-    this.props.mutate({
-      variables: {
-        title: this.state.title
-      }
-    });
+    this.props
+      .mutate({
+        variables: {
+          title: this.state.title
+        }
+      })
+      .then(() => hashHistory.push("/"));
   }
 
   render() {
     return (
       <div>
+        <Link to="/">Back</Link>
         <h3>Create a New Song</h3>
         <form onSubmit={this.onSubmit.bind(this)}>
           <label>Song Title:</label>
